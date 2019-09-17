0f680dbd41a6d5f9f9188a3e89d0b2e527100c5e
lei.tian <lei.tian@uhouzz.com>
2019-09-18 00:40:52 +0800

    feat: 更新测试类/文档

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-### ip2region
+### ip2region SDK for PHP
 
+### Installation
 > composer require chinayin/ip2region
+
+### Quick Examples
+```php
+use lionsoul2014\Ip2Region;
+
+$ip2region = new Ip2Region;
+
+$ip = '220.181.38.150';
+$data = $ip2region->memorySearch($ip);
+$data = $ip2region->binarySearch($ip);
+$data = $ip2region->btreeSearch($ip);
+
+// binary算法/b-tree算法/Memory算法：
+// 0.x毫秒/0.1x毫秒/0.1x毫秒
+// 任何客户端b-tree都比binary算法快，当然Memory算法固然是最快的！
+```
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,26 +7,24 @@ use lionsoul2014\Ip2Region;
 class TestCase extends \PHPUnit\Framework\TestCase
 {
 
-    public function testBinarySearch()
+    public function testFeature()
     {
+        $funs = [
+            'binarySearch', 'memorySearch', 'btreeSearch'
+        ];
         $ip = '220.181.38.150';
+        $expected = [
+            'city_id' => 215,
+            'region' => '中国|0|北京|北京市|电信',
+        ];
         $ip2region = new Ip2Region;
-        $s_time = $this->getTime();
-        $data = $ip2region->binarySearch($ip);
-        $c_time = $this->getTime() - $s_time;
-        printf("%s ==> %s|%s in %.5f millseconds\n", $ip, $data['city_id'], $data['region'], $c_time);
-        $this->assertNotEmpty($data);
-    }
-
-    public function testMemorySearch()
-    {
-        $ip = '220.181.38.150';
-        $ip2region = new Ip2Region;
-        $s_time = $this->getTime();
-        $data = $ip2region->memorySearch($ip);
-        $c_time = $this->getTime() - $s_time;
-        printf("%s ==> %s|%s in %.5f millseconds\n", $ip, $data['city_id'], $data['region'], $c_time);
-        $this->assertNotEmpty($data);
+        foreach ($funs as $fn) {
+            $start = $this->getTime();
+            $data = $ip2region->$fn($ip);
+            $end = $this->getTime() - $start;
+            printf("%s (%s) ==> %s|%s in %.5f millseconds\n", $ip, $fn, $data['city_id'], $data['region'], $end);
+            $this->assertEquals($expected, $data);
+        }
     }
 
     function getTime()
